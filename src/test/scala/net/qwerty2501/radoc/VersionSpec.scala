f09d715ac1e6f8543859fd343227b6fbe2dd8ea5
qwerty2501 <qwerty2501@live.jp>
2017-12-11 23:14:04 +0900

    added exception cases.

--- src/test/scala/net/qwerty2501/radoc/VersionSpec.scala
+++ src/test/scala/net/qwerty2501/radoc/VersionSpec.scala
@@ -11,4 +11,10 @@ class VersionSpec extends FlatSpec with Matchers {
   it should "be 3.2.4.5" in {
     Version(3, 2, 4, 5).toString should be("3.2.4.5")
   }
+
+  "minus number" should "throw IllegalArgumentException" in {
+    intercept[IllegalArgumentException] {
+      Version(-1, -1, -1, -1)
+    }
+  }
 }
