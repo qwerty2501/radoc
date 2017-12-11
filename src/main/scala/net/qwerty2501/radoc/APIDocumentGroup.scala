14a9a0ad587bc828b444def2f8df8010142af810
qwerty2501 <qwerty2501@live.jp>
2017-12-12 00:12:06 +0900

    modified member name.

--- src/main/scala/net/qwerty2501/radoc/APIDocumentGroup.scala
+++ src/main/scala/net/qwerty2501/radoc/APIDocumentGroup.scala
@@ -1,4 +1,4 @@
 package net.qwerty2501.radoc
 
 private case class APIDocumentGroup(group: String,
-                                    requestResponses: Seq[APIDocument])
+                                    apiDocuments: Seq[APIDocument])
