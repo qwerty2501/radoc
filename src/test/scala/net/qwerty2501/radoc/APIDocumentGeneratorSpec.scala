50394e29199799f85f8641458a5f4605e8e2a265
qwerty2501 <qwerty2501@live.jp>
2018-01-03 22:39:33 +0900

    added version 0.0.0.0 case.

--- src/test/scala/net/qwerty2501/radoc/APIDocumentGeneratorSpec.scala
+++ src/test/scala/net/qwerty2501/radoc/APIDocumentGeneratorSpec.scala
@@ -3,17 +3,54 @@ package net.qwerty2501.radoc
 import java.io.File
 import java.nio.file._
 
+import net.qwerty2501.radoc.mocks.APIDocumentBuilderMock
 import org.scalatest._
 
 import scala.io.Source
 
 class APIDocumentGeneratorSpec extends FlatSpec with Matchers {
 
+  it should "can generate only version 0.0.0.0 api document" in {
+    val filePath = "doc/samples/version0.0.0.0document.html"
+    val builder = new APIDocumentBuilderMock()
+    val sampleJson = """
+                      |{
+                      | "member":1,
+                      |}
+                    """.stripMargin
+    val sampleResponse = Response(
+      Status.Ok,
+      sampleJson
+    )
+    builder.append(
+      Request.get("/sample/path"),
+      sampleResponse
+    )
+    builder.append(Request.post("/sample/path", sampleJson), sampleResponse)
+    builder.append(Request.get("/sample/path2"), Response(Status.Ok))
+
+    val path = Paths.get(filePath)
+    Files.deleteIfExists(path)
+    APIDocumentGenerator.generateDocument(builder.getRootAPIDocument, filePath)
+    Files.exists(path) should be(true)
+
+  }
+
+  it should "can generate api document file" in {
+    val filePath = "doc/samples/empty_document.html"
+    val path = Paths.get(filePath)
+    Files.deleteIfExists(path)
+    APIDocumentGenerator.generateDocument(
+      RootAPIDocument("empty doc title", Map()),
+      filePath)
+    Files.exists(path) should be(true)
+  }
+
   it should "can generate api document" in {
     val rootAPIDocument = RootAPIDocument("", Map())
-    APIDocumentGeneratorInternal.generate(
-      rootAPIDocument,
-      APIDocumentGenerateContext()) should not be empty
+
+    APIDocumentGeneratorInternal
+      .generate(rootAPIDocument, APIDocumentGenerateContext()) should not be empty
   }
 
   it should "can output document" in {
