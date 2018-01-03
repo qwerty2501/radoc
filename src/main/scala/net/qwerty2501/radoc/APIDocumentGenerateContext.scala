3f67f75e0c2a69634e65f7479657c6de1c81c041
qwerty2501 <qwerty2501@live.jp>
2018-01-03 22:40:46 +0900

    added template path.

--- src/main/scala/net/qwerty2501/radoc/APIDocumentGenerateContext.scala
+++ src/main/scala/net/qwerty2501/radoc/APIDocumentGenerateContext.scala
@@ -1,7 +1,11 @@
 package net.qwerty2501.radoc
 
-case class APIDocumentGenerateContext(templatePath: String) {
-  def this() = this("net.qwerty2501.radoc/rootAPIDocument.ssp")
+case class APIDocumentGenerateContext(
+    rootAPIDocumentTemplatePath: String,
+    rootAPIDocumentWithVersionTemplatePath: String) {
+  def this() =
+    this("net.qwerty2501.radoc/rootAPIDocument.ssp",
+         "rootAPIDocumentWithVersion.ssp")
 }
 
 object APIDocumentGenerateContext {
