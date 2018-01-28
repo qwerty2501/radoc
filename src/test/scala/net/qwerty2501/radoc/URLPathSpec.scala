3dced6945f07b30c61e7e300c0612c6bb5aac39f
qwerty2501 <qwerty2501@live.jp>
2018-01-28 23:09:53 +0900

    added URLPath Spec

---
+++ src/test/scala/net/qwerty2501/radoc/URLPathSpec.scala
@@ -0,0 +1,23 @@
+package net.qwerty2501.radoc
+
+import org.scalatest._
+
+class URLPathSpec extends FlatSpec with Matchers {
+
+  it should "parameter path" in {
+    val parameter = Parameter("id", 33, Text("id description"))
+    val queryParameter =
+      Parameter("name", "test name", Text("name description"))
+    val queryParameter2 =
+      Parameter("name2", "test name", Text("name description"))
+    val urlPath = URLPath / "test/path" / parameter :? queryParameter & queryParameter2
+
+    urlPath.displayPath should be("/test/path/:id")
+    urlPath.actualPath should be("/test/path/33")
+    urlPath.pathParameters should be(Seq(parameter))
+
+    urlPath.queries.length should be(2)
+    urlPath.queries.head should be(queryParameter)
+    urlPath.queries(1) should be(queryParameter2)
+  }
+}
