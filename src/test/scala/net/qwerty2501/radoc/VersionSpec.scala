05597c646694ff13e6a7e19e2cbfbd382e0fca4d
qwerty2501 <qwerty2501@live.jp>
2018-01-29 03:20:21 +0900

    removed revision field.

--- src/test/scala/net/qwerty2501/radoc/VersionSpec.scala
+++ src/test/scala/net/qwerty2501/radoc/VersionSpec.scala
@@ -4,17 +4,17 @@ import org.scalatest._
 
 class VersionSpec extends FlatSpec with Matchers {
 
-  it should "be 0.0.0.0" in {
-    Version().toString should be("0.0.0.0")
+  it should "be 0.0.0" in {
+    Version().toString should be("0.0.0")
   }
 
-  it should "be 3.2.4.5" in {
-    Version(3, 2, 4, 5).toString should be("3.2.4.5")
+  it should "be 3.2.4" in {
+    Version(3, 2, 4).toString should be("3.2.4")
   }
 
   "minus number" should "throw IllegalArgumentException" in {
     intercept[IllegalArgumentException] {
-      Version(-1, -1, -1, -1)
+      Version(-1, -1, -1)
     }
   }
 }
