c05725d03cd4a77f5a6783d823e75856a16ce5bb
qwerty2501 <qwerty2501@live.jp>
2018-02-10 03:59:12 +0900

    added default

--- radoc-core/src/main/scala/net/qwerty2501/radoc/FieldModifier.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/FieldModifier.scala
@@ -4,18 +4,21 @@ trait FieldModifier {
   private[radoc] def fieldModify(field: String): String
 }
 
-final object FieldModifier {
-  final val None: FieldModifier = field => field
+object FieldModifier {
 
-  final val Snake: FieldModifier = field =>
-  "[A-Z]".r.replaceAllIn("[^A-Z]([A-Z)".r.replaceAllIn(field, { m =>
-    "_" + m.group(1).toLowerCase
-  }), { m =>
-    m.group(0).toLowerCase
-  })
+  private[radoc] val default: FieldModifier = _ => ""
 
-  final val Camel: FieldModifier = field  =>
-  "_([a-z\\d])".r.replaceAllIn(field, { m =>
-    m.group(1).toUpperCase
-  })
+  val None: FieldModifier = field => field
+
+  val Snake: FieldModifier = field =>
+    "[A-Z]".r.replaceAllIn("[^A-Z]([A-Z)".r.replaceAllIn(field, { m =>
+      "_" + m.group(1).toLowerCase
+    }), { m =>
+      m.group(0).toLowerCase
+    })
+
+  val Camel: FieldModifier = field =>
+    "_([a-z\\d])".r.replaceAllIn(field, { m =>
+      m.group(1).toUpperCase
+    })
 }
