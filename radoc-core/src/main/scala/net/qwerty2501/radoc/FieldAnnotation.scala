b5954b3ba5cdf327877387fbe8526e9064f2734f
qwerty2501 <qwerty2501@live.jp>
2018-02-10 20:09:19 +0900

    changed class name. added constructor over load.

--- radoc-core/src/main/scala/net/qwerty2501/radoc/FieldAnnotation.scala
+++
@@ -1,6 +0,0 @@
-package net.qwerty2501.radoc
-
-import scala.annotation.StaticAnnotation
-
-case class FieldAnnotation(parameterHint: ParameterHint)
-    extends StaticAnnotation
---
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/FieldHintAnnotation.scala
@@ -0,0 +1,23 @@
+package net.qwerty2501.radoc
+
+import scala.annotation.StaticAnnotation
+
+case class FieldHintAnnotation(
+    parameter: Parameter,
+    essentiality: Essentiality,
+    defaultParameterAssertFactory: ParameterAssertFactory)
+    extends StaticAnnotation {
+
+  def this(parameter: Parameter,
+           defaultParameterAssertFactory: ParameterAssertFactory) =
+    this(parameter, Essentiality.Mandatory, defaultParameterAssertFactory)
+
+  def this(parameter: Parameter) =
+    this(parameter, ParameterAssertFactory.default)
+
+  def this(description: Text) = this(Parameter("", description))
+}
+
+private object FieldHintAnnotation {
+  val default = new FieldHintAnnotation(Text())
+}
