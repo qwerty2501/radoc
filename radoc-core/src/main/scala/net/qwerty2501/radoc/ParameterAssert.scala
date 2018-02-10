fb87ced583bb046542d9f8e3136fd7be6d53982f
qwerty2501 <qwerty2501@live.jp>
2018-02-10 20:08:16 +0900

    use nameless class.

--- radoc-core/src/main/scala/net/qwerty2501/radoc/ParameterAssert.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/ParameterAssert.scala
@@ -3,49 +3,30 @@ package net.qwerty2501.radoc
 import scala.reflect.ClassTag
 
 trait ParameterAssert {
-  def assert(actual: Any, parameterHint: ParameterHint)
-}
-
-private class ParameterEqualAssert(expected: Any) extends ParameterAssert {
-  override def assert(actual: Any, parameterHint: ParameterHint): Unit =
-    if (expected != actual)
-      throw new AssertionError(
-        "The expected is" + ParameterValue(expected).toString + "but the actual is" + ParameterValue(
-          actual).toString)
-}
-
-private class ParamterTypeEqualAssert(expected: Class[_])
-    extends ParameterAssert {
-  override def assert(actual: Any, parameterHint: ParameterHint): Unit =
-    if (actual != null && expected != actual.getClass)
-      throw new AssertionError(
-        "The expected type is " + expected.getName + "but the actual is not equal " + expected.getClass.getName
-      )
-}
-
-private class ParameterOriginalAssert(
-    assertHandler: ((Any, ParameterHint) => Unit))
-    extends ParameterAssert {
-  override def assert(actual: Any, parameterHint: ParameterHint): Unit =
-    assertHandler(actual, parameterHint)
-}
-
-private class ParameterNothingAssert() extends ParameterAssert {
-  override def assert(actual: Any, parameterHint: ParameterHint): Unit = {}
+  def assert(actual: Any, parameter: Parameter)
 }
 
 object ParameterAssert {
-  private[radoc] val default = new ParameterNothingAssert {}
-  private final val none = new ParameterNothingAssert()
+  private[radoc] val default: ParameterAssert = (_, _) => ()
+  private final val none: ParameterAssert = (_, _) => ()
   def apply(): ParameterAssert = none
   def equalAssert(expected: Any): ParameterAssert =
-    new ParameterEqualAssert(expected)
+    (actual, _) =>
+      if (expected != actual)
+        throw new AssertionError(
+          "The expected is" + ParameterValue(expected).toString + "but the actual is" + ParameterValue(
+            actual).toString)
 
   def typeEqualAssert[T]()(implicit ctg: ClassTag[T]): ParameterAssert =
     typeEqualAssert(ctg.runtimeClass)
-  def typeEqualAssert(expected: Class[_]): ParameterAssert =
-    new ParamterTypeEqualAssert(expected)
 
-  def apply(assertHandler: ((Any, ParameterHint) => Unit)): ParameterAssert =
-    new ParameterOriginalAssert(assertHandler)
+  def typeEqualAssert(expected: Class[_]): ParameterAssert =
+    (actual, _) =>
+      if (actual != null && expected != actual.getClass)
+        throw new AssertionError(
+          "The expected type is " + expected.getName + "but the actual is not equal " + actual.getClass.getName
+      )
+
+  def apply(assertHandler: ((Any, Parameter) => Unit)): ParameterAssert =
+    (actual, parameter) => assertHandler(actual, parameter)
 }
