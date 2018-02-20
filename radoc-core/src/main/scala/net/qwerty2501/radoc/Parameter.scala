c5bbd97cbc5673db9f83297d040d1e859a897414
qwerty2501 <qwerty2501@live.jp>
2018-02-20 21:59:29 +0900

    split ParameterType

--- radoc-core/src/main/scala/net/qwerty2501/radoc/Parameter.scala
+++ radoc-core/src/main/scala/net/qwerty2501/radoc/Parameter.scala
@@ -1,9 +1,25 @@
 package net.qwerty2501.radoc
 
-class Parameter private (val field: String,
-                         val value: Option[_],
-                         val typeName: String,
-                         val description: Text) {
+trait Parameter {
+  val field: String
+  val value: Option[_]
+  val typeName: String
+  val description: Text
+  private[radoc] val color: Color
+}
+
+private class SpecifiedColorParameter(val field: String,
+                                      val value: Option[_],
+                                      val typeName: String,
+                                      val description: Text,
+                                      private[radoc] val color: Color)
+    extends Parameter
+
+private class AutoColorParameter private (val field: String,
+                                          val value: Option[_],
+                                          val typeName: String,
+                                          val description: Text)
+    extends Parameter {
 
   def this(field: String, value: Any, typeName: String, description: Text) =
     this(field, Option(value), typeName, description)
@@ -19,29 +35,36 @@ class Parameter private (val field: String,
 
   def this(field: String, typeClass: Class[_], description: Text) =
     this(field, Option.empty, typeClass.getSimpleName, description)
-  lazy val color: Color = ParameterColor.color
+  private[radoc] lazy val color: Color = ParameterColor.color
 }
 
 object Parameter {
+
+  private[radoc] def apply(field: String,
+                           value: Option[_],
+                           typeName: String,
+                           description: Text,
+                           color: Color): Parameter =
+    new SpecifiedColorParameter(field, value, typeName, description, color)
 
   def apply(field: String,
             value: Any,
             typeName: String,
             description: Text): Parameter =
-    new Parameter(field, value, typeName, description)
+    new AutoColorParameter(field, value, typeName, description)
 
   def apply(field: String,
             value: Option[_],
             typeName: String,
             description: Text): Parameter =
-    new Parameter(field, value, typeName, description)
+    new AutoColorParameter(field, value, typeName, description)
 
   def apply(field: String, value: Option[_], description: Text): Parameter =
-    new Parameter(field, value, description)
+    new AutoColorParameter(field, value, description)
 
   def apply(field: String, value: Any, description: Text): Parameter =
-    new Parameter(field, value, description)
+    new AutoColorParameter(field, value, description)
   def apply(field: String, typeClass: Class[_], description: Text): Parameter =
-    new Parameter(field, typeClass, description)
+    new AutoColorParameter(field, typeClass, description)
 
 }
